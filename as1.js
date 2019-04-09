b1dbc02bf97dd7a19c9a9aaa605a16726cacd079
vabhishek900 <49457583+vabhishek900@users.noreply.github.com>
2019-04-09 16:41:31 -0400

    Add files via upload

---
+++ as1.js
@@ -0,0 +1,45 @@
+function add(a,b,callback)
+
+{
+
+	disp(a);
+
+	disp(b);
+
+}
+
+
+function disp(no)
+
+{
+	if(no%2==0)
+
+	{
+		console.log("The Number is Even"+ no);
+
+	}
+
+	else
+
+	{
+
+		console.log("The Number is Odd"+ no);
+
+	}
+
+}
+
+
+add(10,12);
+
+var i = 0;
+function f1()
+{
+    i++;
+    console.log("Hello Dash!"+i);
+    if(i>9)
+    {
+        clearInterval(timerID)
+    }
+}
+var timerID = setInterval(f1,2000);
